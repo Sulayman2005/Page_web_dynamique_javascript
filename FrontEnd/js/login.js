7c7b0cf3d6ae89cc31347c9bcf01d019a90cb660
John Doe <johndoe@example.com>
2025-01-03 15:31:52 +0100

    acharnement sur le login

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,38 +1,38 @@
-document.getElementById("login_form").addEventListener("submit", async function(e) {
-    e.preventDefault();
+document.getElementById("login_form").addEventListener("submit", async function(event){
+    event.preventDefault();
+
+    const errorDiv = document.getElementById("error");
+    errorDiv.textContent = '';
+    errorDiv.style.color = 'red';
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const errorDiv = document.getElementById("error");
-    errorDiv.textContent = ''; 
-    errorDiv.style.color = 'red';
-
     try {
         const response = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
+            body: JSON.stringify({email, password}),
             headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email, password }),
+                "Content-Type": "application/json"
+            }
         });
-
-        if (!response.ok) {
-            if (response.status === 401) {
+         if (!response.ok) {
+            if(response.status === 401) {
                 errorDiv.textContent = "Erreur dans l'identifiant ou le mot de passe";
             } else {
-                errorDiv.textContent = "Erreur, veuillez réessayer";
+                errorDiv.textContent = "Veuillez réssayer s'il vous plaît!";
+            } if (response.status === 200) {
+                password.innerHTML = "Nom d'utilisateur disponible";
+                password.style.color = "green";
             }
             return;
-        }
+        };
 
         const data = await response.json();
         console.log("Connexion réussie :", data);
 
-        window.location.href = "./index.html";
-
-    } catch (error) {
-        errorDiv.textContent = "Une erreur s'est produite, veuillez réessayer";
-        console.error("Erreur lors de la tentative de connexion :", error);
+        window.location.href = "./index.html"; 
+    } catch(error) {
+        error.console.log("Une erreur est survenue: ", error);
     }
 });
