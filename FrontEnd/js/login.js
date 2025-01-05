53344e55cd60bf5da5b88bb704a93e473f507d96
John Doe <johndoe@example.com>
2025-01-05 15:21:20 +0100

    login réussie

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,38 +1,33 @@
 document.getElementById("login_form").addEventListener("submit", async function(event){
     event.preventDefault();
 
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const api = "http://localhost:5678/api/users/login";
+
     const errorDiv = document.getElementById("error");
     errorDiv.textContent = '';
     errorDiv.style.color = 'red';
+    
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
 
-    try {
-        const response = await fetch("http://localhost:5678/api/users/login", {
+        let response = await fetch(api, {
             method: "POST",
             body: JSON.stringify({email, password}),
             headers: {
                 "Content-Type": "application/json"
             }
         });
-         if (!response.ok) {
-            if(response.status === 401) {
-                errorDiv.textContent = "Erreur dans l'identifiant ou le mot de passe";
-            } else {
-                errorDiv.textContent = "Veuillez réssayer s'il vous plaît!";
-            } if (response.status === 200) {
-                password.innerHTML = "Nom d'utilisateur disponible";
-                password.style.color = "green";
-            }
-            return;
-        };
-
-        const data = await response.json();
-        console.log("Connexion réussie :", data);
-
-        window.location.href = "./index.html"; 
-    } catch(error) {
-        error.console.log("Une erreur est survenue: ", error);
-    }
+        
+        if(response.status =! 200) {
+            errorDiv.textContent = "Identifiant ou mot de passe incorrect";
+            errorDiv.className = "error-login";
+            errorDiv.innerHTML =
+            "Veuillez vérifier votre email et/ou votre mot de passe";
+            document.querySelector("form").prepend(errorBox);
+        } else {
+            let data = await response.json();
+            console.log("Connexion réussie :", data);
+            window.location.href = "./index.html";
+        }
 });
